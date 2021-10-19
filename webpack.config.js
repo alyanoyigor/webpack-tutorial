824e6441354bd429f495c8beb4fad26e82819976
Igor <igoralyanoy@gmail.com>
2021-10-19 17:23:25 +0300

    feat: add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
 	entry: {
@@ -15,5 +16,6 @@ module.exports = {
 		new HTMLWebpackPlugin({
 			template: "./src/index.html",
 		}),
+		new CleanWebpackPlugin(),
 	],
 };
