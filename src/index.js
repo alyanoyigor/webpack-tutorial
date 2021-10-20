a921fa0b00a8197236de6ccd1a5a27efc21464a4
Igor <igoralyanoy@gmail.com>
2021-10-20 18:23:48 +0300

    feat: add file-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { Post } from "./Post";
+import "./style/style.css";
+import json from "./assets/json.json";
 
 let post = new Post("New post 1");
 console.log(post.toString());
+console.log(json);
